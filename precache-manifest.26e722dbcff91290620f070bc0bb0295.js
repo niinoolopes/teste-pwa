3136ff705800c31796c4536b4b9434d33c371280
Anderson Lopes <niinoolopes0@gmail.com>
2022-08-16 13:52:17 -0300

    Add files via upload

---
+++ precache-manifest.26e722dbcff91290620f070bc0bb0295.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4367be34fb15abea6b74",
+    "url": "/css/app.1687b2ee.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "f6a77957419e24afe89fd8cc267fc6a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "306d4d90e794854c04fc",
+    "url": "/js/about.49d91dd9.js"
+  },
+  {
+    "revision": "4367be34fb15abea6b74",
+    "url": "/js/app.e7fa22d1.js"
+  },
+  {
+    "revision": "76fce69b4871eecf6ced",
+    "url": "/js/chunk-vendors.c877a832.js"
+  },
+  {
+    "revision": "6b6a9cb28fd5d797f55d1438210678f9",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
