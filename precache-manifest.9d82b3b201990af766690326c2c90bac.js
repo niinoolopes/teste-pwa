f86888f1ccbd176561049f4dbc772d59945d1b40
nn_araujo <anderson.araujo@d0c88.com.br>
2022-08-16 13:05:47 -0300

    first commit

---
+++ precache-manifest.9d82b3b201990af766690326c2c90bac.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afe78a22bd0244f11089",
+    "url": "/css/app.1687b2ee.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "3af06d10e22ba8bbd60f5ada62577e41",
+    "url": "/index.html"
+  },
+  {
+    "revision": "306d4d90e794854c04fc",
+    "url": "/js/about.49d91dd9.js"
+  },
+  {
+    "revision": "afe78a22bd0244f11089",
+    "url": "/js/app.83630807.js"
+  },
+  {
+    "revision": "76fce69b4871eecf6ced",
+    "url": "/js/chunk-vendors.c877a832.js"
+  },
+  {
+    "revision": "6b6a9cb28fd5d797f55d1438210678f9",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
